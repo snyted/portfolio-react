cb13bcb7ee220015e526e24b7cd5c6d7bc0e54eb
João Vitor Farias <joaofarias_16@hotmail.com>
2025-09-04 17:51:55 -0300

    portfolio-w-react

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+   <div className="bg-blue-100 text-white p-4">
+      <h1 className="text-3xl font-bold">
+        Olá, Tailwind está funcionando!
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
