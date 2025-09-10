8212c30a0fe9fd6b4c65474c833411c5eb961a28
João Vitor Farias <joaofarias_16@hotmail.com>
2025-09-10 12:33:40 -0300

    fix: deploy route

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/portfolio-react/',
   plugins: [react(), tailwindcss()],
 })
