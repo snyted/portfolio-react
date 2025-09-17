e13f37a381384b44d8ef7c101ff9e9838e74c36f
João Vitor Farias <joaofarias_16@hotmail.com>
2025-09-17 15:38:53 -0300

    modify: H2Component to Titles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import Header from "./assets/components/Header";
-import HeroSection from "./assets/section/HeroSection";
+import HeroSection from "./assets/section/Hero";
 import BubbleEffect from "./assets/ui/BubblePurpleEffect";
 import Habilities from "./assets/section/Habilities";
 import Projects from "./assets/section/Projects";
 
 function App() {
   return (
-    <main className="relative min-h-screen bg-gradient-to-br from-purple-800 via-indigo-900 to-black overflow-hidden font-roboto-custom z-0 text-white flex flex-col">
+    <main className="relative min-h-screen bg-gradient-to-br from-purple-800 via-indigo-900 to-black overflow-hidden font-roboto-custom z-0 text-white flex flex-col scroll-smooth">
       <BubbleEffect />
       <Header />
       <HeroSection />
---
+++ src/assets/components/Titles/index.tsx
@@ -0,0 +1,22 @@
+interface TitlesProps {
+  title?: string;
+  text?: string;
+}
+
+export default function Titles({ title, text }: TitlesProps) {
+  return (
+    <div className="flex flex-col gap-1">
+      <h1
+  className="font-bold text-5xl 
+             bg-[linear-gradient(to_left,#9ca3af_45%,white_50%,#9ca3af_55%)]
+             bg-clip-text 
+             text-transparent 
+             bg-[length:200%_100%] 
+             animate-shine text-center"
+>
+  {title}
+</h1>
+      <p className="text-white/40 text-center">{text}</p>
+    </div>
+  );
+}
